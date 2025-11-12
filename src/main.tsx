bfcbee764f105cb8ab506e3f611356cc26010116
matheusrach444 <Www.matheusrach460r@gmail.com>
2025-11-12 18:28:26 -0300

    Atualizar o main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Game } from './components/game/Game';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <Game />
-  </StrictMode>
+  </React.StrictMode>
 );
